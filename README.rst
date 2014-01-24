ff07f7b3d250e5ba56ac4a6c1ff45c707025aff6
kjwon15 <kjwonmail@gmail.com>
2014-01-25 04:38:12 +0900

    first commit

---
+++ README.rst
@@ -0,0 +1,34 @@
+EarthReader-TTY
+===============
+
+EarthReader client for TTY
+
+Distributed under `GPLv2`__ or later
+
+__ http://www.gnu.org/licenses/gpl-2.0.html
+
+
+Install
+-------
+
+You can install Earth Reader for TTY using ``pip``:
+
+.. code-block:: console
+
+   $ pip install git+git://github.com/earthreader/tty.git
+
+
+Links
+-----
+
+Earth Reader
+   http://earthreader.org/
+
+libearth
+   http://github.com/earthreader/libearth/
+
+Git repository (GitHub)
+   http://github.com/earthreader/tty/
+
+Issue tracker (GitHub)
+   http://github.com/earthreader/tty/issues/
---
+++ setup.py
@@ -0,0 +1,47 @@
+import os.path
+import sys
+
+try:
+    from setuptools import find_packages, setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import find_packages, setup
+
+
+def readme():
+    try:
+        with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+            return f.read()
+    except (IOError, OSError):
+        return ''
+
+
+install_requires = [
+    'libearth >= 0.1.1',
+]
+if sys.version_info < (2, 7):
+    install_requires.append('argparse >= 1.2.1')
+
+
+setup(
+    name='EarthReader-TTY',
+    description='Earth Reader for TTY',
+    long_description=readme(),
+    url='http://earthreader.org/',
+    author='Earth Reader team',
+    author_email='earthreader' '@' 'librelist.com',
+    license='AGPLv3 or later',
+    entry_points={
+        'console_scripts': [
+            'ert = earthreader.command:main'
+        ]
+    },
+    packages=find_packages(),
+    install_requires=install_requires,
+    dependency_links=[
+        'https://github.com/earthreader/libearth/archive/master.zip'
+        '#egg=libearth-dev'
+    ],
+    download_url='https://github.com/earthreader/tty/releases',
+)
